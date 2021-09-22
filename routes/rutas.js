46c97756fbd4fac4505b45bf96ab69e51ef1b363
villa10 <joaquin.villalon@mail.udp.cl>
2021-09-22 20:41:35 -0300

    v1.0.0

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,41 +1,50 @@
 const { Router } = require('express');
 const router = Router();
 const fs = require('fs');
+const { ClientRequest } = require('http');
 
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient(6379 ,'127.0.0.1');
 
+// conexion reddis-cli
 client.on("error", function(error) {
-  console.error(error);
+    console.error(error);
 });
 
-//conexion reddis-cli
-client.set("Jose",'3000','arteaga', redis.print);
-const aux2=client.get("Jose", redis.print);
-console.log(aux2);
+router.get('/search?', async (req, res) => {
+    // Ejemplo: localhost:3000/inventory/search?q=cpu
+    let buscar = req.query.q;
+    
+    // Buscamos en cache lo solicitado
+    if (client.exists(buscar,  function(err, reply) {
+        if (reply == 1) {
+            console.log('existe en cache');
+            client.get(buscar, function(err, reply) {
+                let products = JSON.parse(reply);
+                return res.json({
+                    products
+                });
+            });
+        }
+        else {
+            console.log('no existe en cache');
+            // Buscamos en inventario el q solicitado
+            const aux = [];
+            let data = fs.readFileSync('./jsonexam.json');
+            data = JSON.parse(data).products;
+            data.forEach(elemento => {
+                if (elemento.name.includes(buscar)) {
+                    aux.push(elemento);
+                }
+            });
+            client.set(buscar, JSON.stringify(aux), 'EX', 60 * 60 * 24);
 
-
-let data= fs.readFileSync('./jsonexam.json');
-data=JSON.parse(data).products;
-router.get('/search?', (req, res) => {
-	const aux=[];
-	console.log(data);
-    // Ejemplo en postman
-    // localhost:3000/inventory/search?q=cpu
-    	let buscar = req.query.q;
-    // Buscamos en inventario el q solicitado
-	data.forEach(elemento => {
-		if(elemento.name.includes(buscar)){
-				console.log('mash : ',elemento);
-				aux.push(elemento)
-		}
-	});
-	
-
-    // Retornamos al cliente
-    res.json({
-        msg: aux
-    });
+            // Retornamos al cliente
+            return res.json({
+                products: aux
+            });
+        }
+    }));
 });
 
 module.exports = router;
