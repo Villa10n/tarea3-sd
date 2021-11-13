0f09b0a75692a87de2f447944afe9bf1f63de7cf
Villa10n <joaquin.villalon@mail.udp.cl>
2021-11-13 02:18:20 -0300

    tarea2-v0.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+var bodyParser = require('body-parser');
 
 // Crear el server
 const app = express();
@@ -7,8 +8,13 @@ const app = express();
 // Public
 app.use(express.static('public'));
 
+// urlencode
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Rutas
-app.use('/inventory', require('./routes/rutas'));
+app.use('/tarea2', require('./routes/rutas'));
+
 
 // Peticiones
 app.listen(process.env.PORT, () => {
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,50 +1,39 @@
 const { Router } = require('express');
 const router = Router();
-const fs = require('fs');
-const { ClientRequest } = require('http');
+const kafka = require('kafka-node');
 
-const redis = require("redis");
-const client = redis.createClient(6379 ,'127.0.0.1');
 
-// conexion reddis-cli
-client.on("error", function(error) {
-    console.error(error);
+router.post('/orders', async (req, res) => {
+    const data = JSON.stringify(req.body);
+
+    // Conexiones
+    const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+    var producer = new kafka.Producer(client);
+    
+    // Guardamos en orders
+    producer.on('ready', async function () {
+        await producer.send( [ { topic: "orders", messages: data }], function (err,data) {console.log('enviado')} )
+    });
+
+    return res.status(200).json({
+        ok: true,
+        msg: 'orden ingresada'
+    });
 });
 
-router.get('/search?', async (req, res) => {
-    // Ejemplo: localhost:3000/inventory/search?q=cpu
-    let buscar = req.query.q;
-    
-    // Buscamos en cache lo solicitado
-    if (client.exists(buscar,  function(err, reply) {
-        if (reply == 1) {
-            console.log('existe en cache');
-            client.get(buscar, function(err, reply) {
-                let products = JSON.parse(reply);
-                return res.json({
-                    products
-                });
-            });
-        }
-        else {
-            console.log('no existe en cache');
-            // Buscamos en inventario el q solicitado
-            const aux = [];
-            let data = fs.readFileSync('./jsonexam.json');
-            data = JSON.parse(data).products;
-            data.forEach(elemento => {
-                if (elemento.name.includes(buscar)) {
-                    aux.push(elemento);
-                }
-            });
-            client.set(buscar, JSON.stringify(aux), 'EX', 60 * 60 * 24);
-
-            // Retornamos al cliente
-            return res.json({
-                products: aux
-            });
-        }
-    }));
+router.get('/DailySummary', async (req, res) => {
+    const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+    var consumer = new kafka.Consumer(client, [ { topic: 'orders' } ]);
+    await consumer.on('message', function (message) {
+    	console.log(message);
+	});
+    consumer.commit(function(err, data) {
+        return res.status(200).json({
+            ok: true,
+            msg: 'daily'
+        });
+    });
+    // Desconectemos al consumidor
 });
 
 module.exports = router;
