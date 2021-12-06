c362c847d3570a5cf41a74b657b2a1db351b7d77
JoseArteaga <62229012+JoseArteaga@users.noreply.github.com>
2021-12-05 22:40:22 -0300

    update rutas.js

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,14 +1,34 @@
 const { Router } = require('express');
 const router = Router();
+const { Pool } = require('pg')
+
+const connectionMaster={
+	user:'user1',
+	host:'localhost',
+	database:'postgres',
+	password:'password1',
+	port: 55432,
+}
+
+const connectionSlave={
+	user:'user1',
+	host:'localhost',
+	database:'postgres',
+	password:'password1',
+	port: 65432,
+}
+
 
 router.get('/GetProduct', async (req, res) => {
-    try {
-        console.log(process.pid);
-        return res.status(200).json({
-            ok: true,
-            msg: 'GetProduct'
-        });
-    } catch (error) {
+	try {
+		const pool = new Pool(connectionSlave);
+		resp = await pool.query('select * from producto');
+		console.log(resp.rows);
+		return res.status(200).json({
+		    ok: false,
+		    msg:resp.rows,
+		});
+	} catch (error) {
         console.log(error);
         return res.status(200).json({
             ok: false,
@@ -19,10 +39,15 @@ router.get('/GetProduct', async (req, res) => {
 
 router.post('/addProduct', async (req, res) => {
     try {
+    	const{nombre,precio}=req.body;
+    	const pool = new Pool(connectionMaster);
+    	
+        resp = await pool.query("insert into producto(nombre,precio) values($1,$2)",[nombre,precio]);
+
         console.log(process.pid);
         return res.status(200).json({
             ok: true,
-            msg: 'addProduct'
+            msg: 'addProduct Exitoso'
         });
     } catch (error) {
         console.log(error);
